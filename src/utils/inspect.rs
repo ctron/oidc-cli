91a355554c9112f544624a1f791b47955f4f7e16
Jens Reimann <ctron@dentrassi.de>
2024-12-05 09:26:21 +0100

    feat: if the token seems binary, show as hexdump

--- src/utils/inspect.rs
+++ src/utils/inspect.rs
@@ -1,5 +1,6 @@
 use biscuit::{Base64Url, CompactPart};
 use colored_json::to_colored_json_auto;
+use pretty_hex::pretty_hex;
 use serde_json::Value;
 use std::io::{stdout, Write};
 
@@ -21,7 +22,12 @@ pub fn inspect_part(part: Base64Url) -> anyhow::Result<()> {
     match serde_json::from_slice::<Value>(&data) {
         Err(err) => {
             println!(" Invalid JSON: {err}");
-            stdout().lock().write_all(&data)?;
+            if is_text(&data) {
+                stdout().lock().write_all(&data)?;
+            } else {
+                let output = pretty_hex(&data);
+                stdout().lock().write_all(output.as_bytes())?;
+            }
         }
         Ok(value) => {
             println!();
@@ -29,4 +35,8 @@ pub fn inspect_part(part: Base64Url) -> anyhow::Result<()> {
         }
     }
     Ok(())
+}
+
+fn is_text(data: &[u8]) -> bool {
+    String::from_utf8(data.to_vec()).is_ok()
 }
